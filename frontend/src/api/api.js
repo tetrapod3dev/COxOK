07944134a0ccf0ea16d87a634039d3fdfabbf6f0
Taerok <sdf7575@naver.com>
2020-08-05 14:54:29 +0900

    Add input tag 'insert tags' in blogPostMakeView.vue

--- frontend/src/api/api.js
+++ frontend/src/api/api.js
@@ -1,35 +1,35 @@
 export default {
-  URL: 'http://i3a104.p.ssafy.io',
+  URL: "http://i3a104.p.ssafy.io",
   ROUTES: {
     // Accounts ROUTES
-    signup: '/user/signup/',
-    login: '/user/login/',
-    checkEmail: '/user/email/',
-    checkNickname: '/user/nickname/',
-    myPage: '/user/mypage/', // 회원 정보 리턴 페이지..?
-    update: '/user/modify/', // 회원 정보 수정
-    withdrawal: '/user/withdrawal/', // 회원 탈퇴
+    signup: "/user/signup/",
+    login: "/user/login/",
+    checkEmail: "/user/email/",
+    checkNickname: "/user/nickname/",
+    myPage: "/user/mypage/", // 회원 정보 리턴 페이지..?
+    update: "/user/modify/", // 회원 정보 수정
+    withdrawal: "/user/withdrawal/", // 회원 탈퇴
 
     // Recipes ROUTES
-    recipeList: '/recipe/all/', // 모든 레시피 리스트를 받기 위한 주소(뒤에 페이지 주소 0부터 시작 / 6개씩 전달)
-    searchRecipe: '/recipe/search/', // 레시피 검색을 위한 주소입니다. (선택된 카테고리의 id와 재료의 id를 Array로 보냅니다.)
+    recipeList: "/recipe/all/", // 모든 레시피 리스트를 받기 위한 주소(뒤에 페이지 주소 0부터 시작 / 6개씩 전달)
+    searchRecipe: "/recipe/search/", // 레시피 검색을 위한 주소입니다. (선택된 카테고리의 id와 재료의 id를 Array로 보냅니다.)
 
-    recipeDetail: '/recipe/view/', // 레시피 상세 정보를 받기 위한 주소(뒤에 recipe id 붙여주기)
+    recipeDetail: "/recipe/view/", // 레시피 상세 정보를 받기 위한 주소(뒤에 recipe id 붙여주기)
 
-    goRegister: '/recipe/goRegister/', // categories와 ingredients 리스트를 받기 위해 요청을 보내는 주소
-    recipeRegister: '/recipe/register/', // 레시피 작성을 위해 데이터를 보내야 되는 곳
+    goRegister: "/recipe/goRegister/", // categories와 ingredients 리스트를 받기 위해 요청을 보내는 주소
+    recipeRegister: "/recipe/register/", // 레시피 작성을 위해 데이터를 보내야 되는 곳
 
-    recipeDelte: '/recipe/delete/', // 뒤에 recipe_id 붙여주기
+    recipeDelte: "/recipe/delete/", // 뒤에 recipe_id 붙여주기
 
-    recipeLike: '/recipe/like/',
-    recipeDislike: '/recipe/likeCancel/',
+    recipeLike: "/recipe/like/",
+    recipeDislike: "/recipe/likeCancel/",
 
-
-    recipeReview: '/review/view/', // 레시피 아이디를 보내면 레시피의 리뷰들이 반환되는 것같아요.
-    reviewRegister: '/review/', // 리뷰 작성을 위한 데이터를 전달할 주소
-    reviewDelete: '/review/', // 뒤에 review_id 붙여주기
+    recipeReview: "/review/view/", // 레시피 아이디를 보내면 레시피의 리뷰들이 반환되는 것같아요.
+    reviewRegister: "/review/", // 리뷰 작성을 위한 데이터를 전달할 주소
+    reviewDelete: "/review/", // 뒤에 review_id 붙여주기
 
     // Blog ROUTES
-    blogRegister: '/blog/',
-  }
-}
+    blogRegister: "/blog/",
+    blogList: "/blog/all/", // 모든 블로그 게시글을 받기 위한 주소
+  },
+};
--- frontend/src/views/blog/BlogPostMakeView.vue
+++ frontend/src/views/blog/BlogPostMakeView.vue
@@ -1,5 +1,6 @@
 <template>
   <div class="wrapper">
+    <!-- 페이지 배너 시작 -->
     <div class="page-header page-header-mini">
       <parallax
         class="page-header-image"
@@ -9,32 +10,47 @@
         <h1>블로그 게시글 작성 페이지</h1>
       </div>
     </div>
-    <div class="section section-about-us">
+    <!-- 페이지 배너 끝 -->
+
+    <!-- 게시글 작성 시작 -->
+    <div class="section">
       <div class="container">
         <div class="row justify-content-md-center" id="row_style">
-          <div class="form-group">
-            <input
-              type="text"
-              class="form-control"
-              placeholder="제목"
-              v-model="blogPost.title"
-            />
-          </div>
+          <!-- 제목 -->
+          <input type="text" class="form-control" placeholder="제목" v-model="blogPost.title" />
 
+          <!-- 텍스트 에디터 -->
           <CxkEditor v-on:content="contentHandler" />
+          <!-- 태그 시작 -->
+          <b-form-tags v-model="blogTags" no-outer-focus class="mb-2">
+            <template v-slot="{ tags, inputAttrs, inputHandlers, tagVariant, addTag, removeTag }">
+              <b-input-group v-show="blogTags.length < 3" class="mb-2">
+                <b-form-input v-bind="inputAttrs" v-on="inputHandlers" class="form-control"></b-form-input>
+                <b-input-group-append>
+                  <b-button @click="addTag()" variant="primary">Add</b-button>
+                </b-input-group-append>
+              </b-input-group>
+              <div class="d-inline-block" style="font-size: 1.5rem;">
+                <b-form-tag
+                  v-for="tag in tags"
+                  @remove="removeTag(tag)"
+                  :key="tag"
+                  :title="tag"
+                  :variant="tagVariant"
+                  class="mr-1"
+                >{{ tag }}</b-form-tag>
+              </div>
+            </template>
+          </b-form-tags>
         </div>
-        <b-form-tags
-          input-id="tags-basic"
-          v-model="blogTags"
-          class="mb-2"
-        ></b-form-tags>
+
         <div class="form-group">
-          <button @click="preTest" class="btn btn-primary" id="submit">
-            등록
-          </button>
+          <button @click="preTest" class="btn btn-primary" id="submit">등록</button>
+          <button @click="preTest" class="btn btn-default" id="submit">취소</button>
         </div>
       </div>
     </div>
+    <!-- 게시글 작성 끝 -->
   </div>
 </template>
 
@@ -53,9 +69,9 @@ export default {
       blogPost: {
         title: "",
         content: "",
-        tag1: "",
-        tag2: "",
-        tag3: "",
+        tag1: null,
+        tag2: null,
+        tag3: null,
         recipe_id: null,
       },
     };
@@ -65,6 +81,9 @@ export default {
   },
   computed: {
     ...mapGetters(["config"]),
+    tagState() {
+      return this.blogPost.length > 2 && this.blogPost.legnth < 0;
+    },
   },
   methods: {
     contentHandler(event) {
@@ -92,10 +111,21 @@ export default {
           Authorization: this.config,
         },
       };
-      console.log("INPUT START");
-      console.log(SERVER.URL + SERVER.ROUTES.blogRegister);
+
+      this.blogPost.tag1 = null;
+      this.blogPost.tag2 = null;
+      this.blogPost.tag3 = null;
+      if (this.blogTags.length > 2) {
+        this.blogPost.tag3 = this.blogTags[2];
+      }
+      if (this.blogTags.length > 1) {
+        this.blogPost.tag2 = this.blogTags[1];
+      }
+      if (this.blogTags.length > 0) {
+        this.blogPost.tag1 = this.blogTags[0];
+      }
+
       console.log(this.blogPost);
-      console.log("INPUT END");
       axios
         .post(SERVER.URL + SERVER.ROUTES.blogRegister, this.blogPost, configs)
         .then((res) => {
