bfa40451c291b270602d54c6b632cb4d4301b85b
장윤정 <wkddbswjd325@gmail.com>
2020-08-07 14:03:30 +0900

    FIX warning

--- backend/src/main/java/com/ssafy/cookblog/dao/RecipePhotoDaoImpl.java
+++ backend/src/main/java/com/ssafy/cookblog/dao/RecipePhotoDaoImpl.java
@@ -7,7 +7,6 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Repository;
 
 import com.ssafy.cookblog.dto.RecipePhotoDto;
-import com.ssafy.cookblog.dto.UserDto;
 
 @Repository
 public class RecipePhotoDaoImpl implements RecipePhotoDao {
