97c109af072d6cbd839c9d778d7331a2399c8241
장윤정 <wkddbswjd325@gmail.com>
2020-08-10 13:19:13 +0900

    ADD ReportService,Dao

--- backend/src/main/java/com/ssafy/cookblog/controller/ReportController.java
+++ backend/src/main/java/com/ssafy/cookblog/controller/ReportController.java
@@ -11,16 +11,14 @@ import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import com.ssafy.cookblog.dto.VersusDto;
-import com.ssafy.cookblog.dto.response.VersusResponseDto;
+import com.ssafy.cookblog.dto.ReportRecipeDto;
+import com.ssafy.cookblog.service.ReportService;
 import com.ssafy.cookblog.service.UserService;
-import com.ssafy.cookblog.service.VersusService;
 import com.ssafy.cookblog.util.JwtService;
 
 import io.swagger.annotations.ApiOperation;
---
+++ backend/src/main/java/com/ssafy/cookblog/dto/ReportRecipeDto.java
@@ -0,0 +1,16 @@
+package com.ssafy.cookblog.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class ReportRecipeDto {
+	private long recipeId;
+	private long userId;
+	private String reason;
+}
