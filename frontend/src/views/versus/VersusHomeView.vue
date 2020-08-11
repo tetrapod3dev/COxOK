58e8b73005358486b4599b73e39964ed7c2dbb47
Donghyeok-Lee <lee33843@gmail.com>
2020-08-11 12:49:48 +0900

    FIX versus detail

--- frontend/src/views/versus/VersusHomeView.vue
+++ frontend/src/views/versus/VersusHomeView.vue
@@ -1,7 +1,10 @@
 <template>
   <div class="wrapper">
     <div class="page-header page-header-mini">
-      <h2 class="my-5">Test2</h2>
+      <parallax
+        class="page-header-image"
+        style="background-image: url('https://cdn.pixabay.com/photo/2017/07/28/13/29/spices-2548653_960_720.jpg') ;"
+      ></parallax>
 
       <div class="content-center">
         <h1 class="title">요리대전</h1>
@@ -16,7 +19,7 @@
     <div class="container">
       <div class="row">
         <router-link
-          class="col-12 col-lg-4 col-md-6"
+          class="col-4"
           v-for="versus in versusList"
           :key="versus.versusId"
           :to="{name: 'VersusDetailView', params: {versus_id: versus.versusId} }"
@@ -35,6 +38,7 @@
     </div>
 
     <div id="bottomSensor"></div>
+    <button @click="scrollToTop" class="button-bottom">^</button>
   </div>
 </template>
 
@@ -85,6 +89,9 @@ export default {
         }, 1000);
       });
     },
+    scrollToTop: function () {
+      window.scrollTo({ top: 0, left: 0, behavior: "smooth" });
+    },
     loadUntilViewportIsFull: function () {
       const bottomSensor = document.querySelector("#bottomSensor");
       const watcher = scrollmonitor.create(bottomSensor);
@@ -95,7 +102,13 @@ export default {
   },
   created: function () {
     this.getVersus();
-  }
+  },
+  mounted: function () {
+    this.addScrollWatcher();
+  },
+  updated: function () {
+    this.loadUntilViewportIsFull();
+  },
 };
 </script>
 
