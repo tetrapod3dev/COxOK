36ea6436be5f57dece9508ee6a51b162bdb93fef
장윤정 <wkddbswjd325@gmail.com>
2020-08-18 11:45:23 +0900

    ADD online meet join list

--- frontend/src/views/clubs/ClubOnlineDetailView.vue
+++ frontend/src/views/clubs/ClubOnlineDetailView.vue
@@ -22,19 +22,20 @@
     </section>
 
     <div class="container">
-
+      <h3 class="meet-subtitle text-left col-12">[{{ online.type }}]</h3>
       <div v-if="online.video != null">
         <div class="embed-responsive embed-responsive-16by9">
           <iframe class="embed-responsive-item" :src="videoUrl" allowfullscreen></iframe>
         </div>
       </div>
       <div v-else>
-        <a :href="online.link" target="_blank">강의 링크로 이동 {{ online.link }}</a>
+        <div class="live-div col-12">
+          <p class="meet-content text-left col-12">강의 링크: <a :href="online.link" target="_blank"> {{ online.link }}</a></p>
+          <p class="meet-content text-left col-12">날짜: {{ simpleDate(online.date) }}</p>
+          <p class="meet-content text-left col-12">시간: {{ simpleTime(online.date) }}</p>
+        </div>
       </div>
       
-      <p class="meet-subtitle text-left col-12">타입: {{ online.type }}</p>
-      <p class="meet-content text-left col-12">날짜: {{ simpleDate(online.date) }}</p>
-      <p class="meet-content text-left col-12">시간: {{ simpleTime(online.date) }}</p>
             
       <div v-if="author.id != userId" class="join-btn-div">
         <button class="btn" style="width:100%" @click="changeJoinClub">{{ joinMent }}</button>
@@ -43,6 +44,22 @@
       <div class="col-12 mt-5">
         <h3 class="text-left">상세 내용</h3>
         <div class="detail-main-content" v-html="online.content" />
+      </div>
+
+      <div class="col-12">
+        <h3 class="text-left mt-5">찜한 사람 ({{ joinList.length }})</h3>
+        
+        <div class="row">
+        <p
+            v-for="(user, index) in joinList"
+            :key="user.userId"
+            class="col-2"
+          ><img :id="getUserIndex(index)" class="join-member-profile img-raised mb-5" :src="userProfile(index)" />
+            <b-tooltip :target="getUserIndex(index)" triggers="hover" placement="bottom">
+              {{user.nickname}}
+            </b-tooltip>
+          </p>
+        </div>
       </div>
 
       <router-link
@@ -84,6 +101,7 @@ export default {
         thumbnailSrc: "dochi.png",
         video: null,
       },
+      joinList: [],
       author: {
         id: null,
         nickname: null,
@@ -101,11 +119,16 @@ export default {
       return `https://youtube.com/embed/${this.online.video}`;
     },
     joinMent() {
-      return this.isIn ? "취소하기" : "신청하기";
+      return this.isIn ? "찜 취소하기" : "강의 찜하기";
     },
   },
-  created() {
-    axios
+  created() {},
+  mounted() {
+    this.getInfo();
+  },
+  methods: {
+    getInfo() {
+      axios
       .get(
         SERVER.URL + SERVER.ROUTES.onlineDetail + this.$route.params.club_id,
         {
@@ -121,8 +144,13 @@ export default {
           nickname: res.data.writerNickname,
         };
         this.userId = res.data.userId;
+        this.joinList = res.data.joinList;
         this.isIn =
-          res.data.joinList.indexOf(res.data.userId) >= 0 ? true : false;
+            res.data.joinList
+              .map((user) => user.userId)
+              .indexOf(res.data.userId) >= 0
+              ? true
+              : false;
       })
       .catch((err) => {
         if (err.response.status == 401) {
@@ -130,8 +158,8 @@ export default {
           this.logout()
         }});
 
-  },
-  methods: {
+    },
+
     deleteClub() {
       let response = confirm("삭제하실 건가요?");
       if (response) {
@@ -173,7 +201,7 @@ export default {
             }
           )
           .then(() => {
-            alert("참가를 취소하셨습니다.");
+            alert("찜 목록에서 삭제하셨습니다.");
             this.isIn = false;
           })
           .catch((err) => console.log(err.response));
@@ -189,7 +217,7 @@ export default {
             }
           )
           .then(() => {
-            alert("참가 신청에 성공했습니다.");
+            alert("강의를 찜 하셨습니다.");
             this.isIn = true;
           })
           .catch((err) => {
@@ -199,6 +227,7 @@ export default {
             }});
 
       }
+      this.getInfo();
     },
     simpleDate(date) {
       return date.slice(0,10);
@@ -207,7 +236,7 @@ export default {
       return date.slice(11,16);
     },
     userProfile(index) {
-      return SERVER.IMAGE_URL + this.meet.meetJoinList[index].profilePhoto;
+      return SERVER.IMAGE_URL + this.joinList[index].profilePhoto;
     },
     getUserIndex(index){
       return "user-profile" + index;
@@ -312,5 +341,15 @@ button.learn-more:active::before {
 
 button.submit:active::before {
   box-shadow: 0 0 0 2px #77AF9C, 0 0 #cff0da;
+}
+
+.join-btn-div, .live-div {
+  font-size:20px;
+}
+
+.join-member-profile {
+  width:60px;
+  height:60px;
+  border-radius:100%;
 }
 </style>
